db39522cae6680df19cad96b2b082e3f49e54790
d1scrd <u21473103@tuks.co.za>
2024-05-19 18:36:28 +0200

    Fixed the issue with stylings not being displayed

--- Desktop/src/App.svelte
+++ Desktop/src/App.svelte
@@ -1,11 +1,6 @@
-<script>
-  let name = 'world';
-</script>
-
-<style>
-  h1 {
-    @apply text-4xl text-blue-500;
-  }
-</style>
-
-<h1>Hello {name}!</h1>
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 border-8">
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
