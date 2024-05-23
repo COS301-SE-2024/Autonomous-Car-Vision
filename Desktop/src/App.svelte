5ed4f928939517d2ccd169f83057f2fbda0c74ab
Donatello G Carboni <u22534599@tuks.co.za>
2024-05-23 10:26:28 +0200

    Added needed files for linting

--- Desktop/src/App.svelte
+++ Desktop/src/App.svelte
@@ -1,6 +1,8 @@
-<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 border-8">
+<div
+  class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 border-8"
+>
   <div>
     <div class="text-xl font-medium text-black">ChitChat</div>
-    <p class="text-slate-500">You have a new message!</p>
+    <p class="text-slate-500">You have a new message! Lol</p>
   </div>
 </div>
