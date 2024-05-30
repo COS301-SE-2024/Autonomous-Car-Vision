fc3ebe7f9958c273c6f21bbe8e092ae637f33a29
JordanTimberlake <u21565882@tuks.co.za>
2024-05-30 18:10:22 +0200

    Updated text input because of Svelte materialify

--- Desktop/src/components/SignUpForm.svelte
+++ Desktop/src/components/SignUpForm.svelte
@@ -1,78 +1,81 @@
 <script>
-    import navigate from "svelte-spa-router";
+    import { Button, TextField, Icon, MaterialApp } from "svelte-materialify";
+    import { mdiEyeOff, mdiEye } from "@mdi/js";
+
+    function handleEnterdown(e) {
+        if(e.key == "Enter"){
+            onSubmit();
+        }
+	}
+
+    let username_Email = '';
+    let password = ''
+    let confirmPassword = ''
 
     const onSubmit = () => {
-        console.log("Login button clicked");
-        console.log(document.getElementsByName("uName-Email").value);
-        console.log(document.getElementById("uname-email").value);
-
-        console.log(document.getElementById("password").value);
-        console.log(document.getElementById("Cpassword").value);
-
+        console.log("Login");
+        console.log(username_Email);
+        console.log(password);
+        console.log(confirmPassword);
     };
+
+    let show = false;
+    let showConfirm = false;
 </script>
 
 <div class="w-10/12 mx-auto py-4 my-4">
-    <div class="flex flex-row gap-2">
-        <a
-            class="w-full h-14 flex flex-col flex-wrap justify-center items-center bg-gray-200 rounded-sm"
-            href="#/login"
-            on:click={() => navigate("/login")}
-        >
-            <button>Log In</button>
-        </a>
-        <a
-            class="w-full h-14 flex flex-col flex-wrap justify-center items-center bg-gradient-to-r from-green-300 to-green-600 rounded-sm"
-            href="#/signup"
-            on:click={() => navigate("/signup")}
-        >
-            <button>Sign Up</button>
-        </a>
-    </div>
-    <div class="w-full p-4 border-2 rounded-md border-green-500 mt-2">
-        <div class="text-left">
-            <h1 class="text-2xl">Welcome!</h1>
-            <p class="text-base">Please enter your information.</p>
-            <p class="text-sm">Sign up with a username or email.</p>
+    <MaterialApp>
+        <div class="flex flex-row gap-2">
+            <a
+                class="w-full h-14 flex flex-col flex-wrap justify-center items-center"
+                href="#/login"
+            >
+                <Button class="text-black bg-gray-200" depressed block>Log In</Button>
+            </a>
+            <a
+                class="w-full h-14 flex flex-col flex-wrap justify-center items-center"
+                href="#/signup"
+            >
+                <Button class="text-black bg-gradient-to-r from-green-300 to-green-600" depressed block>Sign Up</Button>
+            </a>
         </div>
-        <div id="form" class="flex flex-col gap-1 py-2">
-            <!-- <label for="uName-Email">Username or Email</label> -->
-            <input
-                type="text"
-                id="uname-email"
-                name="uName-Email"
-                placeholder="Username or Email"
-            />
-            <!-- <label for="uName-Email">Password</label> -->
-            <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password..."
-            />
-            <input
-                type="password"
-                id="Cpassword"
-                name="Cpassword"
-                placeholder="Confirm password..."
-            />
+        <div class="w-full p-4 border-2 rounded-md border-green-500 mt-2">
+            <div class="text-left">
+                <h1 class="text-2xl">Welcome!</h1>
+                <p>Please enter your information to sign up.</p>
+            </div>
+            <div on:keydown={handleEnterdown} id="form" class="flex flex-col gap-1 py-2">
+                <!-- <label for="uName-Email">Username or Email</label> -->
+                <TextField bind:value={username_Email} outlined>Username/Email</TextField>
+                <!-- <label for="uName-Email">Password</label> -->
+                <TextField bind:value={password} outlined type={show ? "text" : "password"}>
+                    Password
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <div
+                        slot="append"
+                        on:click={() => {
+                            show = !show;
+                        }}
+                    >
+                        <Icon path={show ? mdiEyeOff : mdiEye} />
+                    </div>
+                </TextField>
+                <TextField bind:value={confirmPassword} outlined type={showConfirm ? "text" : "password"}>
+                    Confirm password
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <div
+                        slot="append"
+                        on:click={() => {
+                            showConfirm = !showConfirm;
+                        }}
+                    >
+                        <Icon path={showConfirm ? mdiEyeOff : mdiEye} />
+                    </div>
+                </TextField>
+            </div>
+            <Button class="bg-green-400" on:click={onSubmit} rounded block
+                >Log in</Button
+            >
         </div>
-        <button
-            class="w-full p-1 my-2 bg-blue-500 rounded-md"
-            on:click={onSubmit}>Register</button
-        >
-    </div>
+    </MaterialApp>
 </div>
-
-<style>
-    input {
-        border-color: rgb(115, 255, 115);
-        border-radius: 0.375rem;
-        border: 2px solid rgb(115, 255, 115) !important;
-        padding: 0.25rem;
-    }
-
-    input:focus {
-        border: 2px solid rgb(115, 255, 115) !important;
-    }
-</style>
