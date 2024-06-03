fb92315386605109d0e42e38d64b2e722c59f0ff
d1scrd <u21473103@tuks.co.za>
2024-06-03 04:03:43 +0200

    Gallery integration

--- Desktop/src/preload.js
+++ Desktop/src/preload.js
@@ -12,5 +12,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     hashPassword: (password) => ipcRenderer.invoke('hash-password', password),
-    hashPasswordSalt: (password,salt) => ipcRenderer.invoke('hash-password-salt', password, salt)
+    hashPasswordSalt: (password,salt) => ipcRenderer.invoke('hash-password-salt', password, salt),
+    insertData: (record) => ipcRenderer.invoke('insert-data', record),
+    selectData: (mname) => ipcRenderer.invoke('select-data', mname),
+    updateData: (mid, updates) => ipcRenderer.invoke('ureq', mid, updates),
+    uploadFile: (filePath, mid, uid, token, mediaName) => ipcRenderer.invoke('upload-file', filePath, mid, uid, token, mediaName),
 });
