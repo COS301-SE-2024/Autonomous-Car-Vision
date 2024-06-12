23ddefbb85ae718f19e8515e08f3032fde7db2e2
keamothapo <u21543462@tuks.co.za>
2024-06-12 09:23:33 +0200

    TIMELINE COMPONENT

---
+++ Desktop/src/components/TimelineModel.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let Model = {
+    
+    profileImage : '',
+    modelName : '',
+    lastUsedTimestamp : '',
+    modelVersion : '',
+
+  };
+
+</script>
+
+<div class="timeline-component">
+    <img src={Model.profileImage} alt="Profile Image" class="profile-image" />
+    <div class="details">
+      <span class="model-name">{Model.modelName}</span>
+      <span class="timestamp">Last used: {Model.lastUsedTimestamp}</span>
+    </div>
+    <div class="version">
+      v{Model.modelVersion}
+    </div>
+ 
+</div>
