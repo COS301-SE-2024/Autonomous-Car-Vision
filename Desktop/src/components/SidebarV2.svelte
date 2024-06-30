0422a525947a84874fdfbbf37ab6156cb4ff801f
JordanTimberlake <u21565882@tuks.co.za>
2024-06-30 21:58:45 +0200

    Added adjustable sidebar.

---
+++ Desktop/src/components/SidebarV2.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { location } from "svelte-spa-router";
+
+    let routes = [
+        {
+            name: "Gallery",
+            route: "#/gallery"
+        },
+        {
+            name: "Upload",
+            route: "#/upload"
+        },
+        {
+            name: "Models",
+            route: "#/models"
+        },
+        {
+            name: "Help",
+            route: "#/help"
+        },
+    ]
+
+</script>
+
+<div class="h-screen w-full bg-theme-blue-primary text-white">
+    {#each routes as route}
+        <div class="nav-item {("#" + $location) === route.route ? 'active' : ''}">
+            <a href={route.route}>
+                <div>{route.name}</div>
+            </a>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .nav-item {
+        display: flex;
+        justify-content: center;
+        padding: 10px 0 10px 0;
+    }
+
+    .active>a>div {
+        color: blue;
+    }
+</style>
--- Desktop/src/routes/ProtectedRoutes.svelte
+++ Desktop/src/routes/ProtectedRoutes.svelte
@@ -4,6 +4,7 @@
   import { onMount } from "svelte";
   import { navigate } from "@sveltejs/kit/navigation"; // If using SvelteKit for navigation
   import Sidebar from "../components/Sidebar.svelte";
+  import SidebarV2 from "../components/SidebarV2.svelte";
 
   let authToken;
 
@@ -19,14 +20,73 @@
       navigate("#login"); // Change this to your login route
     }
   });
+
+  let sidebarWidth = 200; // initial width of the sidebar
+  const minOffset = 200;
+  const maxOffset = 500;
+
+  function handleMouseMove(event) {
+    let offset = event.pageX;
+
+    offset = offset < minOffset ? minOffset : offset;
+    offset = offset > maxOffset ? maxOffset : offset;
+
+    sidebarWidth = offset;
+  }
+
+  function handleMouseUp() {
+    window.removeEventListener('mousemove', handleMouseMove);
+    window.removeEventListener('mouseup', handleMouseUp);
+  }
+
+  function handleMouseDown() {
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+  }
+
+  onMount(() => {
+    // Cleanup event listeners if the component is unmounted
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  });
 </script>
 
 <!-- Slot to render the protected content if authenticated -->
-<div class="grid grid-cols-6 h-screen">
-  <div class="col-span-1">
-    <Sidebar />
-  </div>
-    <div class="col-span-5">
-      <slot />
+<div class="h-full w-full">
+  <div class="side">
+    <div class="sidebar" style="width: {sidebarWidth}px;">
+      <SidebarV2 />
     </div>
+  <div class="handle" on:mousedown={handleMouseDown}></div>
+  </div>
+  <div class="content" style="margin-left: {sidebarWidth + 15}px;">
+    <slot />
+  </div>
 </div>
+
+<style>
+  .side {
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+  .sidebar {
+    float: left;
+    height: 100%;
+    background: #ffcdd2;
+    overflow: auto;
+  }
+  .handle {
+    float: left;
+    width: 15px;
+    height: 100vh;
+    background: #0C3E63;
+    cursor: ew-resize;
+  }
+  .content {
+    height: 100%;
+    overflow: auto;
+  }
+</style>
