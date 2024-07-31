1b9950b7292f6b297de0bd52f40462970cf7d860
Jordan Timberlake <u21565882@tuks.co.za>
2024-07-31 10:31:23 +0200

    Update: Svelvet nodes are getting there.

--- Desktop/src/components/ProcessingNode.svelte
+++ Desktop/src/components/ProcessingNode.svelte
@@ -1,77 +1,123 @@
 <script>
-    import { Node, Anchor, Slider, RadioGroup } from "svelvet";
-    import { generateInput, generateOutput } from "svelvet";
-    
-    /**
-     * @typedef {Object} InputStructure
-     * @property {number} value1
-     * @property {number} value2
-     * @property {string} option
-     */
-  
-    /**
-     * @type {InputStructure}
-     */
-    let inputStructure = {
-      value1: 0,
-      value2: 0,
-      option: "default",
-    };
-  
-    // Create initial values for your parameters
-    /**
-     * @type {InputStructure}
-     */
-    const initialData = {
-      value1: 10,
-      value2: 30,
-      option: "multiply",
-    };
-  
-    // Generate a formatted inputs store
-    const inputs = generateInput(initialData);
-  
-    // Specify processor function
-    /**
-     * @param {InputStructure} inputs
-     * @returns {number}
-     */
-    const processor = (inputs) => {
-      if (inputs.option === "add") {
-        return inputs.value1 + inputs.value2;
-      } else if (inputs.option === "subtract") {
-        return inputs.value1 - inputs.value2;
-      } else if (inputs.option === "multiply") {
-        return inputs.value1 * inputs.value2;
-      } else {
-        return inputs.value1 / inputs.value2;
-      }
-    };
-  
-    // Generate output store
-    const output = generateOutput(inputs, processor);
-  </script>
-  
-  <Node width={400} height={200} useDefaults>
-    <div class="node">
-      <div class="radio-group">
-        <RadioGroup
-          options={["subtract", "add", "multiply", "divide"]}
-          parameterStore={$inputs.option}
-        />
-      </div>
-      <div class="sliders">
-        <Slider parameterStore={$inputs.value1} />
-        <Slider parameterStore={$inputs.value2} />
-      </div>
-      <div class="input-anchors">
-        {#each Object.entries($inputs) as [key, value] (key)}
-          <Anchor bgColor="green" {key} inputsStore={inputs} input />
-        {/each}
-      </div>
-      <div class="output-anchors">
-        <Anchor bgColor="red" outputStore={output} output />
-      </div>
+  import {
+    Node,
+    Anchor,
+    Slider,
+    RadioGroup,
+    Edge,
+    generateInput,
+    generateOutput,
+  } from "svelvet";
+
+  /**
+   * @typedef {Object} InputStructure
+   * @property {number} value1
+   * @property {number} value2
+   * @property {string} option
+   */
+
+  /**
+   * @type {InputStructure}
+   */
+  let inputStructure = {
+    value1: 0,
+    value2: 0,
+    option: "default",
+  };
+
+  // Create initial values for your parameters
+  /**
+   * @type {InputStructure}
+   */
+  const initialData = {
+    value1: 10,
+    value2: 30,
+    option: "multiply",
+  };
+
+  // Generate a formatted inputs store
+  const inputs = generateInput(initialData);
+
+  // Specify processor function
+  /**
+   * @param {InputStructure} inputs
+   * @returns {number}
+   */
+  const processor = (inputs) => {
+    if (inputs.option === "add") {
+      return inputs.value1 + inputs.value2;
+    } else if (inputs.option === "subtract") {
+      return inputs.value1 - inputs.value2;
+    } else if (inputs.option === "multiply") {
+      return inputs.value1 * inputs.value2;
+    } else {
+      return inputs.value1 / inputs.value2;
+    }
+  };
+
+  export let Identifier = "";
+  export let connectors = [];
+  export let position = { x: 0, y: 0 };
+  console.log(Identifier)
+
+  // Generate output store
+  const output = generateOutput(inputs, processor);
+</script>
+
+<Node
+  position={position}
+  id={Identifier}
+  connections={connectors}
+  width={400}
+  height={200}
+  useDefaults
+>
+  <div class="node">
+    <div class="radio-group">
+      <RadioGroup
+        options={["add", "subtract", "multiply", "divide"]}
+        parameterStore={$inputs.option}
+      />
     </div>
-  </Node>
-  
+    <div class="sliders">
+      <Slider parameterStore={$inputs.value1} />
+      <Slider parameterStore={$inputs.value2} />
+    </div>
+    <div class="input-anchors">
+      {#each Object.entries($inputs) as [key, value] (key)}
+        <Anchor bgColor="green" {key} inputsStore={inputs} input />
+      {/each}
+    </div>
+    <div class="output-anchors">
+      <Anchor
+        direction="east"
+        id={Identifier}
+        bgColor="red"
+        outputStore={output}
+        output
+      >
+        <Edge slot="edge" color="yellow" label={$output} />
+      </Anchor>
+    </div>
+  </div>
+</Node>
+
+<style>
+  .input-anchors {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    top: 40%;
+    left: 0px;
+  }
+
+  .output-anchors {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
--- Desktop/src/routes/Svelvet.svelte
+++ Desktop/src/routes/Svelvet.svelte
@@ -1,83 +1,112 @@
 <script>
   import ProtectedRoutes from "./ProtectedRoutes.svelte";
   import ProcessingNode from "../components/ProcessingNode.svelte";
-  import { Node, Svelvet, Minimap, Controls, Group, generateInput, generateOutput } from "svelvet";
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
+  import {
+    Node,
+    Svelvet,
+  } from "svelvet";
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
 
   let nodes = writable([
-      { id: 'alpha', position: { x: 0, y: 0 }, bgColor: 'red', label: 'Default Node' },
-      { id: 'beta', position: { x: 0, y: -50 }, bgColor: 'blue', label: 'Test 2nd' }
+    {
+      id: "sourceNode",
+      position: { x: 1000, y: 300 },
+      bgColor: "red",
+      label: "Node id: sourceNode",
+    },
+    {
+      id: "targetNode",
+      position: { x: 1000, y: 500 },
+      bgColor: "blue",
+      label: "Result id: targetNode",
+    },
   ]);
   let nodeIdCounter = 2;
 
   let nodeTypes = [
-      { type: 'Multiply', label: 'Multiply Unit', bgColor: 'lightblue' },
-      { type: 'Divide', label: 'Divide Unit', bgColor: 'lightgreen' },
-      { type: 'Add', label: 'Add Unit', bgColor: 'lightcoral' },
-      { type: 'Subtract', label: 'Subtract Unit', bgColor: 'lightyellow' }
+    { type: "Multiply", label: "Multiply Unit", bgColor: "lightblue" },
+    { type: "Divide", label: "Divide Unit", bgColor: "lightgreen" },
+    { type: "Add", label: "Add Unit", bgColor: "lightcoral" },
+    { type: "Subtract", label: "Subtract Unit", bgColor: "lightyellow" },
   ];
 
   function addNode(type) {
-      const nodeType = nodeTypes.find(t => t.type === type);
-      if (nodeType) {
-          nodes.update(currentNodes => [
-              ...currentNodes,
-              { id: `node-${nodeIdCounter++}`, position: { x: Math.random() * 500, y: Math.random() * 500 }, bgColor: nodeType.bgColor, label: nodeType.label }
-          ]);
-      }
+    const nodeType = nodeTypes.find((t) => t.type === type);
+    if (nodeType) {
+      nodes.update((currentNodes) => [
+        ...currentNodes,
+        {
+          id: `node-${nodeIdCounter++}`,
+          position: { x: Math.random() * 500, y: Math.random() * 500 },
+          bgColor: nodeType.bgColor,
+          label: nodeType.label,
+        },
+      ]);
+    }
   }
 
-  let test = generateInput({ value1: 5, value2: 10, option: 'default' });
 </script>
 
 <ProtectedRoutes>
   <div class="toolbar">
-      <select on:change="{e => addNode(e.target.value)}">
-          <option value="" disabled selected>Select Node Type</option>
-          {#each nodeTypes as nodeType}
-              <option value="{nodeType.type}">{nodeType.label}</option>
-          {/each}
-      </select>
+    <select on:change={(e) => addNode(e.target.value)}>
+      <option value="" disabled selected>Select Node Type</option>
+      {#each nodeTypes as nodeType}
+        <option value={nodeType.type}>{nodeType.label}</option>
+      {/each}
+    </select>
   </div>
-    <div>
-        <button on:click={() => console.log("")}>Output Node</button>
-    </div>  
   <div class="canvas">
-      <Svelvet id="my-canvas" TD minimap controls editable>
-        <Group color="lightblue" groupName="myGroup" position={{x: 500, y: 500}}  width={600} height={200}>
-            <Node/>
-            <Node />
-        </Group>
-            <Node label="Output" />
-        <ProcessingNode/>
-          {#each $nodes as node}
-              <Node id={node.id} position={node.position} bgColor={node.bgColor} label={node.label} inputsStore={test} LR />
-          {/each}
-      </Svelvet>
+    <Svelvet fitView id="my-canvas" TD minimap controls editable>
+      <ProcessingNode
+        connectors={["processingNode"]}
+        Identifier="inputNode"
+        position={{ x: 0, y: 200 }}
+      />
+      <ProcessingNode
+        connectors={["processingNode"]}
+        Identifier="inputNode1"
+        position={{ x: 0, y: 400 }}
+      />
+      <ProcessingNode
+        connectors={["sourceNode", "targetNode"]}
+        Identifier="processingNode"
+        position={{ x: 485, y: 315 }}
+      />
+      {#each $nodes as node}
+        <Node
+          id={node.id}
+          position={node.position}
+          bgColor={node.bgColor}
+          label={node.label}
+          LR
+        />
+      {/each}
+    </Svelvet>
   </div>
 </ProtectedRoutes>
 
 <style>
   .toolbar {
-      margin: 10px;
-      color: white;
+    margin: 10px;
+    color: white;
   }
 
-    .toolbar select {
-        padding: 5px;
-        font-size: 16px;
-    }
+  .toolbar select {
+    padding: 5px;
+    font-size: 16px;
+  }
 
-    .toolbar option {
-        color: black;
-    }
+  .toolbar option {
+    color: black;
+  }
   .canvas {
-      width: 100%;
-      height: 90%;
-      display: flex;
-      justify-content: center;
-      border: 1px solid #ccc;
-      margin-top: 10px;
+    width: 100%;
+    height: 90%;
+    display: flex;
+    justify-content: center;
+    border: 1px solid #ccc;
+    margin-top: 10px;
   }
 </style>
