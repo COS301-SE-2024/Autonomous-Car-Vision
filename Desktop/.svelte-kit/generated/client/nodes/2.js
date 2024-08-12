799bace59fb9f41639b1093a156dfe69fa754253
JordanTimberlake <u21565882@tuks.co.za>
2024-08-12 08:26:49 +0200

    Building app works
    - Package.json was overwritten somewhere

--- Desktop/.svelte-kit/generated/client/nodes/2.js
+++ Desktop/.svelte-kit/generated/client/nodes/2.js
@@ -1,3 +1,3 @@
-import * as universal from "../../../../src/routes/video/[videoUrl]/+page.js";
+import * as universal from "../../../../src/routes/drives/[videoUrl]/+page.js";
 export { universal };
-export { default as component } from "../../../../src/routes/video/[videoUrl]/+page.svelte";
+export { default as component } from "../../../../src/routes/drives/[videoUrl]/+page.svelte";
