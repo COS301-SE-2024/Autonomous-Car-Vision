e15feedfac2d842dc7744bbaa45959124d3b2546
keamothapo <u21543462@tuks.co.za>
2024-08-16 09:37:01 +0200

    team view page completed

---
+++ Desktop/src/components/RemoveMember.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+  
+    export let teamMember;
+    const dispatch = createEventDispatcher();
+  
+    function closePopup() {
+    dispatch("closePopup");
+  }
+
+    function removeMember() {
+      dispatch("cancel");
+    }
+  
+  </script>
+  
+  <div class="fixed inset-0 flex items-center justify-center bg-modal z-50">
+    <div
+      class="bg-theme-dark-background p-6 rounded-lg shadow-lg border border-theme-keith-primary w-1/4"
+    >
+      <div class="flex flex-col boder border-theme-dark-backgroundBlue">
+        <p class="text-md">
+          Are you sure you would like to remove this member from your team?
+        </p>
+        <div class="flex mt-4 space-x-4">
+          <button
+            on:click={closePopup}
+            class="font-medium px-4 py-2 bg-theme-dark-primary text-white rounded"
+            >Cancel</button
+          >
+          <button
+            on:click={removeMember}
+            class="font-medium px-4 py-2 bg-theme-dark-error text-white rounded"
+            >Delete</button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+  
