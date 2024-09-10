1f62d4d7e64aec71cbd3f3d533532a70c95bbb86
keamothapo <u21543462@tuks.co.za>
2024-09-10 14:15:46 +0200

    final final final

--- Desktop/src/routes/Home.svelte
+++ Desktop/src/routes/Home.svelte
@@ -74,14 +74,14 @@
             Developer
           </button>
         </a>
-
+<!-- 
         <a href="#/install" class="w-full">
           <button
             class="w-full py-2 bg-theme-dark-primary text-theme-dark-lightText rounded-lg  transition"
           >
             WIP
           </button> 
-        </a>
+        </a> -->
       </div>
     </div>
 
